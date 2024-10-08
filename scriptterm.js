e00f675eb807ec5bd58eb7948f1315de7aee4baf
Sumit Kumar <sumitkumarjaimahakaal@gmail.com>
2024-10-08 18:51:52 +0530

    CONVERTER

---
+++ scriptterm.js
@@ -0,0 +1,20 @@
+let term = document.querySelector("#term");
+
+term.addEventListener("click", () => {
+    // console.log("hellow world");
+    window.open('term&condition.html');
+});
+
+let Signup = document.querySelector("#signup");
+
+Signup.addEventListener("click", () => {
+    alert("Welcome Back Plese Enter Correct User Name And Pasword")
+});
+
+let Login = document.querySelector("#login");
+
+Login.addEventListener("click", () => {
+    alert("Thanks For Choice Us Plese Enter Corect Detail For Our Security!")
+});
+
+
